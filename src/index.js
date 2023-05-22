bb41f6461f6245fa47796082ac39b11b8d44ebfc
andhra21231 <andhrarifqi@icloud.com>
2023-05-22 10:37:09 +0700

    gatau lagi

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-/*!
+/**
 =========================================================
-* Muse Ant Design Dashboard - v1.0.0
+* Material Dashboard 2 React - v2.1.0
 =========================================================
-* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-* Coded by Creative Tim
-=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "App";
+
+// Material Dashboard 2 React Context Provider
+import { MaterialUIControllerProvider } from "context";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
   </BrowserRouter>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
