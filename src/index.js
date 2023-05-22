bc4645471bd451605087d251078542aeb594ac1b
andhra21231 <andhrarifqi@icloud.com>
2023-05-22 10:22:46 +0700

    Fullfiled Change UI

--- src/index.js
+++ src/index.js
@@ -1,41 +1,22 @@
 /*!
-
 =========================================================
-* Argon Dashboard Chakra - v1.0.0
+* Muse Ant Design Dashboard - v1.0.0
 =========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-chakra
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-chakra/blob/master/LICENSE.md)
-
-* Design and Coded by Simmmple & Creative Tim
-
+* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+* Coded by Creative Tim
 =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-
-import AuthLayout from "layouts/Auth.js";
-import AdminLayout from "layouts/Admin.js";
-import RTLLayout from "layouts/RTL.js"; // Chakra imports
-import { ChakraProvider } from "@chakra-ui/react";
-// Custom Chakra theme
-import theme from "theme/theme.js";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <ChakraProvider theme={theme} resetCss={false} position="relative">
-    <HashRouter>
-      <Switch>
-        <Route path={`/auth`} component={AuthLayout} />
-        <Route path={`/admin`} component={AdminLayout} />
-        <Route path={`/rtl`} component={RTLLayout} />
-        <Redirect from={`/`} to="/admin/dashboard" />
-      </Switch>
-    </HashRouter>
-  </ChakraProvider>,
-  document.getElementById("root")
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"),
 );
